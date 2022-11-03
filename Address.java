96d4ddc28fb7a93ddc5640c2928968ca982400d5
boming141140 <69752753+boming141140@users.noreply.github.com>
2022-11-03 18:09:13 -0400

    Add files via upload

--- Address.java
+++ Address.java
@@ -1,14 +1,15 @@
 public class Address {
-    private String name, city, zipCode;
+    private String name, city;
+    int zipCode;
     private int numOfStreet;
     public Address(){
         name = "";
         numOfStreet = 0;
         city = "";
-        zipCode = "";
+        zipCode = 0;
     }
 
-    public Address(String name, int numOfStreet, String city, String zipCode) {
+    public Address(String name, int numOfStreet, String city, int zipCode) {
         this.name = name;
         this.numOfStreet = numOfStreet;
         this.city = city;
@@ -31,11 +32,11 @@ public class Address {
         this.city = city;
     }
 
-    public String getZipCode() {
+    public int getZipCode() {
         return zipCode;
     }
 
-    public void setZipCode(String zipCode) {
+    public void setZipCode(int zipCode) {
         this.zipCode = zipCode;
     }
 
@@ -48,11 +49,11 @@ public class Address {
     }
 
     public void printAddress() {
-        System.out.printf("Name: %s\n Number of street: %d\n city: %s\n zipCode: %s",name, numOfStreet, city, zipCode);
+        System.out.printf("Street: %s\nNumber of street: %d\ncity: %s\nzipCode: %s\n",name, numOfStreet, city, zipCode);
     }
 
     public boolean isEqualAddress(Address obj) {
-        return name.equals(obj.getName()) && numOfStreet == obj.getNumOfStreet() && city.equals(obj.name) && zipCode.equals(obj.getZipCode());
+        return name.equals(obj.getName()) && numOfStreet == obj.getNumOfStreet() && city.equals(obj.name) && zipCode == obj.getZipCode();
     }
 
 
--- ticketOrder.java
+++ ticketOrder.java
@@ -52,21 +52,64 @@ public class ticketOrder {
         return name.equals(obj.getName()) && address.isEqualAddress(obj.getAddress());
     }
 
+    static boolean isDigit(String inp) {
+        for(int i = 0; i < inp.length(); i++) {
+            if(!Character.isDigit(inp.charAt(i)))
+                return false;
+        }
+        return true;
+    }
     public static void main(String []args) {
+        final int MAX_TICKET_NUM = 1;
+        int count = 0;
         String name,nameS, city, zipCode, numOfTicket, numberOfStreet;
+        ArrayList<String> nameList = new ArrayList<>();
+        ArrayList<ticketOrder> trueTicket = new ArrayList<>();
         ArrayList<ticketOrder> ticketStore = new ArrayList<>();
         Scanner input = new Scanner(System.in);
-        System.out.println("Enter a name:");
-        name = input.nextLine();
-        System.out.println("Enter an address, address name:");
-        nameS = input.nextLine();
-        System.out.println("Enter the numberOfStreet:");
-        numberOfStreet = input.nextLine();
-        System.out.println("Enter city name:");
-        city = input.nextLine();
-        System.out.println("Enter the zipCode:");
-        zipCode = input.nextLine();
-        System.out.println("Enter numOfTicket:");
-        numOfTicket = input.nextLine();
+        while(true) {
+            if (count >= MAX_TICKET_NUM) {
+                System.out.println("Maximum number of tickets have been reached.");
+                break;
+            }
+            System.out.println("Enter a name:");
+            name = input.nextLine();
+            System.out.println("Enter an address, address name:");
+            nameS = input.nextLine();
+            System.out.println("Enter the numberOfStreet:");
+            numberOfStreet = input.nextLine();
+            System.out.println("Enter city name:");
+            city = input.nextLine();
+            System.out.println("Enter the zipCode:");
+            zipCode = input.nextLine();
+            System.out.println("Enter numOfTicket:");
+            numOfTicket = input.nextLine();
+            if(!isDigit(name) && !isDigit(nameS) && !isDigit(city) && isDigit(zipCode) && isDigit(numOfTicket) && isDigit(numberOfStreet)){
+                int numOfS = Integer.parseInt(numberOfStreet);
+                int numOfT = Integer.parseInt(numOfTicket);
+                int zipCodeR = Integer.parseInt(zipCode);
+                Address userAdd = new Address(nameS,numOfS,city,zipCodeR);
+                ticketOrder userTick = new ticketOrder(name,userAdd,numOfT);
+                ticketStore.add(userTick);
+            }else{
+                System.out.println("Invalid Input, Try Again.");
+            }
+            count++;
+        }
+        for (ticketOrder ticketOrder : ticketStore) {
+            boolean hasName = true;
+            if (!nameList.contains(ticketOrder.getName())) {
+                nameList.add(ticketOrder.getName());
+            } else {
+                hasName = false;
+            }
+            if (hasName) {
+                if (ticketOrder.getNumberOfTicket() < 5) {
+                    trueTicket.add(ticketOrder);
+                }
+            }
+        }
+        for(ticketOrder i : trueTicket)
+            i.PrintTicketOrder();
     }
 }
